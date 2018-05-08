d612b49a6f2bf07530250b79d3bef02e67c471be
传说般的悟空 <haowukong@qq.com>
2018-05-08 19:52:20 +0800

    upgrade

--- PAT乙级（Basic level）/1010. 一元多项式求导 (25).cpp
+++ PAT乙级（Basic level）/1010. 一元多项式求导 (25).cpp
@@ -9,15 +9,19 @@
 
 #include <iostream>
 using namespace std;
-int main() {
+int main()
+{
     int a, b, flag = 0;
-    while (cin >> a >> b) {
-        if (b != 0) {
+    while (cin >> a >> b)
+    {
+        if (b != 0)
+        {
             if (flag == 1) cout << " ";
             cout << a * b << " " << b - 1;
             flag = 1;
         }
     }
     if (flag == 0) cout << "0 0";
+    //system("pause");
     return 0;
 }
--- PAT甲级（Advanced level）/1002. A+B for Polynomials (25).cpp
+++ PAT甲级（Advanced level）/1002. A+B for Polynomials (25).cpp
@@ -1,3 +1,20 @@
+1002. A+B for Polynomials (25)
+This time, you are supposed to find A+B where A and B are two polynomials.
+
+Input
+
+Each input file contains one test case. Each case occupies 2 lines, and each line contains the information of a polynomial: K N1 aN1 N2 aN2 ... NK aNK, where K is the number of nonzero terms in the polynomial, Ni and aNi (i=1, 2, ..., K) are the exponents and coefficients, respectively. It is given that 1 <= K <= 10，0 <= NK < ... < N2 < N1 <=1000.
+
+Output
+
+For each test case you should output the sum of A and B in one line, with the same format as the input. Notice that there must be NO extra space at the end of each line. Please be accurate to 1 decimal place.
+
+Sample Input
+2 1 2.4 0 3.2
+2 2 1.5 1 0.5
+Sample Output
+3 2 1.5 1 2.9 0 3.2
+
 #include<iostream>
 using namespace std;
 int main()
@@ -22,6 +39,6 @@ int main()
 	cout << rp;
 	for (int i =1000; i>=0; --i)
 		if (m[i] != 0) printf(" %d %.1f", i, m[i]);
-	system("pause");
+	//system("pause");
 	return 0;
 }
